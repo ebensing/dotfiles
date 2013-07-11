2f2561d02054010247e47260e4f840a389bbcd22
EJ Bensing <ebensing@rm-dash-r.com>
2013-07-11 15:48:23 -0700

    testing

---
+++ test.js
@@ -0,0 +1,10 @@
+// this is purely for testing the benchmark framework, going to delete soon
+
+var o = {
+  totalDocs : 1000,
+  dps : 800,
+  time : "5 minutes"
+};
+
+console.log(JSON.stringify(o));
+process.exit();
